81a73f84630bdb4b0a072d4f42bb5389a6a6b6ac
Nick DiRienzo <dirienzo.n@gmail.com>
2011-05-24 16:08:13 -0400

    jQuery functions do not work as intended. The postId does not change on click.

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 #Author: Nick DiRienzo
 #Began On: 05.22.2011
-import web, os
+import web, os, datetime
 from datetime import date
 
 urls = ('/', 'index', '/blog', 'blog')
@@ -21,7 +21,7 @@ class blog:
 			
 		postTitles = []
 		for i in xrange(0, len(postsFromFile), 3): #gets post titles
-			postTitles.append(postsFromFile[i])
+			postTitles.append(postsFromFile[i].rstrip().upper())
 			
 		postDates = []
 		for i in xrange(1, len(postsFromFile), 3): #gets post dates
@@ -37,32 +37,9 @@ class blog:
 		
 	#Converts number of month to name of month
 	def getMonthName(self, monthNum):
-		if monthNum == 1:
-			return 'January'
-		elif monthNum == 2:
-			return 'February'
-		elif monthNum == 3:
-			return 'March'
-		elif monthNum == 4:
-			return 'April'
-		elif monthNum == 5:
-			return 'May'
-		elif monthNum == 6:
-			return 'June'
-		elif monthNum == 7:
-			return 'July'
-		elif monthNum == 8:
-			return 'August'
-		elif monthNum == 9:
-			return 'September'
-		elif monthNum == 10:
-			return 'October'
-		elif monthNum == 11:
-			return 'November'
-		elif monthNum == 12:
-			return 'December'
-		else:
-			return 'Invalid Month'
+		months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+		'August', 'September', 'October', 'November', 'December']
+		return months[monthNum-1]
 		
 if __name__ == '__main__':
 	app.run()
